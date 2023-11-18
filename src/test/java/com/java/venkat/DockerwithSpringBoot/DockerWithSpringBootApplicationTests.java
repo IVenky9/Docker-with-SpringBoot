64c57c81cec0338a4db5001b8c9c72f015eb8f00
IVenky9 <venkat.techie9@gmail.com>
2023-11-18 22:03:30 +0530

    Initial code commit

---
+++ src/test/java/com/java/venkat/DockerwithSpringBoot/DockerWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.venkat.DockerwithSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
